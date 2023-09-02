662e28c125f645d3f3880afad5819ae97fe126bf
Arseni <arseni_mars97@hotmail.com>
2023-09-02 22:16:11 +0200

    Added functionality to translate to analog, finetune and test after N seconds

--- finetune_digital_on_analog.py
+++ finetune_digital_on_analog.py
@@ -22,8 +22,9 @@ from typing import Optional, Type
 PATH_DATASET = os.path.join("data", "DATASET")
 
 # Training parameters.
-EPOCHS = 20
+EPOCHS = 4
 BATCH_SIZE = 64
+DEVICE = torch.device("cpu")
 
 def load_digital_model():
 # Modify the original model to use the custom layers
@@ -90,10 +91,138 @@ def digital_to_analog(model):
 
     # drift compensation
     rpu_config.drift_compensation = GlobalDriftCompensation()
+    model.load_state_dict(torch.load('model_checkpoint.pth', map_location="cpu"))
 
-    analog_model = convert_to_analog(model, rpu_config, weight_scaling_omega=1.0)
+    return model, rpu_config
 
-    return analog_model
+def create_sgd_optimizer(model, lr):
+    """Create the analog-aware optimizer.
+
+    Args:
+        model (nn.Module): model to be trained.
+    Returns:
+        nn.Module: optimizer
+    """
+    optimizer = AnalogSGD(model.parameters(), lr=lr)
+    optimizer.regroup_param_groups(model)
+
+    return optimizer
+
+def train(model, train_set, epsilon=1e-1):
+    """Train the network.
+
+    Args:
+        model (nn.Module): model to be trained.
+        train_set (DataLoader): dataset of elements to use as input for training.
+    """
+    classifier = nn.NLLLoss()
+    lr = 0.01
+    optimizer = create_sgd_optimizer(model, lr)
+    scheduler = StepLR(optimizer, step_size=40, gamma=0.5)
+
+    time_init = time()
+    for epoch_number in range(EPOCHS):
+        total_loss = 0
+        for images, labels in train_set:
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            optimizer.zero_grad()
+            # Add training Tensor to the model (input).
+            output = model(images)
+            loss = classifier(output, labels)
+            penalty = 0.0
+            for param in model.parameters():
+                penalty += torch.sum((epsilon - torch.abs(param)) * (torch.abs(param) < epsilon))
+            
+            loss = loss + penalty
+            # Run training (backward propagation).
+            loss.backward()
+
+            # Optimize weights.
+            optimizer.step()
+
+            for module in model.modules():
+                if isinstance(module, BitSlicedLinear):
+                    consolidated_weights = module.get_consolidated_weights()
+                    module.transfer_weights(consolidated_weights)
+
+            total_loss += loss.item()
+
+        print("Epoch {} - Training loss: {:.16f}".format(epoch_number, total_loss / len(train_set)))
+        
+        # Decay learning rate if needed.
+        scheduler.step()
+
+    print("\nTraining Time (s) = {}".format(time() - time_init))
+
+def test_evaluation(model, val_set):
+    """Test trained network
+
+    Args:
+        model (nn.Model): Trained model to be evaluated
+        val_set (DataLoader): Validation set to perform the evaluation
+    """
+    # Save initial state of the model for resetting before each drift operation.
+
+    model.eval()
+
+    predicted_ok = 0
+    total_images = 0
+
+    for images, labels in val_set:
+        # Predict image.
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+
+        pred = model(images)
+
+        _, predicted = torch.max(pred.data, 1)
+        total_images += labels.size(0)
+        predicted_ok += (predicted == labels).sum().item()
+
+    print("\nFinal Number Of Images Tested = {}".format(total_images))
+    print("Final Model Accuracy = {}".format(predicted_ok / total_images))
+
+def test_evaluation_future(model, val_set):
+    """Test trained network
+
+    Args:
+        model (nn.Model): Trained model to be evaluated
+        val_set (DataLoader): Validation set to perform the evaluation
+    """
+    # Save initial state of the model for resetting before each drift operation.
+    initial_state = model.state_dict()
+    model.eval()
+
+    for t_inference in [0.0, 1.0, 20.0, 1000.0, 1e5]:
+        # Reset the model to its initial state.
+        model.load_state_dict(initial_state)
+        # Apply drift to the weights.
+        model.drift_analog_weights(t_inference)
+
+        # Setup counter of images predicted to 0.
+        predicted_ok = 0
+        total_images = 0
+
+        for images, labels in val_set:
+            # Predict image.
+            images = images.to(DEVICE)
+            labels = labels.to(DEVICE)
+
+            pred = model(images)
+
+            _, predicted = torch.max(pred.data, 1)
+            total_images += labels.size(0)
+            predicted_ok += (predicted == labels).sum().item()
+
+        print("Number Of Images Tested at t={} = {}".format(t_inference, total_images))
+        print("Model Accuracy at t={} = {}".format(t_inference, predicted_ok / total_images))
+
+    print("\nFinal Number Of Images Tested = {}".format(total_images))
+    print("Final Model Accuracy = {}".format(predicted_ok / total_images))
+
+
 
 def main():
     """Train a PyTorch analog model with the MNIST dataset."""
@@ -101,7 +230,12 @@ def main():
     train_dataset, validation_dataset = load_images()
 
     model = load_digital_model()
-    model.load_state_dict(torch.load('model_checkpoint.pth', map_location="cuda"))
+    model, rpu_config = digital_to_analog(model)
+    test_evaluation(model, validation_dataset)
+    analog_model = convert_to_analog(model, rpu_config)
+    test_evaluation(analog_model, validation_dataset)
+    train(analog_model, train_dataset)
+    test_evaluation_future(analog_model, validation_dataset)
 
 if __name__ == "__main__":
     # Execute only if run as the entry point into the program.
