2b4d2171e21a27081d507e544c387f3d180c211e
Arseni <arseni_mars97@hotmail.com>
2023-09-09 17:30:08 +0200

    Working self-repair but problems in factorized-weight representations with combined weights

--- lib_analog.py
+++ lib_analog.py
@@ -1,6 +1,7 @@
 import torch
+from torch import nn
     
-from aihwkit.nn import AnalogLinear, AnalogSequential
+from aihwkit.nn import AnalogLinear, AnalogSequential, AnalogConv2d
 from aihwkit.optim import AnalogSGD
 from aihwkit.simulator.configs import SingleRPUConfig, ConstantStepDevice, InferenceRPUConfig, WeightModifierType, WeightClipType, WeightNoiseType
 from aihwkit.inference import PCMLikeNoiseModel, GlobalDriftCompensation
@@ -8,7 +9,48 @@ from aihwkit.simulator.rpu_base import cuda
 from aihwkit.nn.conversion import convert_to_analog
 from config import DEVICE
 
-def convert_to_analog(model, rpu_config):
+class DriftMonitor:
+    def __init__(self, model):
+        self.model = model
+        self.initial_outputs = {}
+
+    def run_identity_through_layer(self, layer):
+        if isinstance(layer, AnalogLinear):
+            identity_input = torch.eye(layer.in_features).unsqueeze(0).to(DEVICE)
+        elif isinstance(layer, AnalogConv2d):
+            in_channels = layer.in_channels
+            kernel_size = layer.kernel_size[0]
+            identity_input = torch.zeros((1, in_channels, kernel_size, kernel_size)).to(DEVICE)
+            for i in range(min(in_channels, kernel_size)):
+                identity_input[0, i, i, i] = 1.0
+        return layer(identity_input)
+
+    def record_initial_outputs(self):
+        for name, layer in self.model.named_children():
+            if isinstance(layer, (AnalogLinear, AnalogConv2d)):
+                self.initial_outputs[name] = self.run_identity_through_layer(layer)
+
+    def check_drift(self, threshold=1e-6):
+        drifted_layers = {}
+        for name, layer in self.model.named_children():
+            if isinstance(layer, (AnalogLinear, AnalogConv2d)) and name in self.initial_outputs:
+                current_output = self.run_identity_through_layer(layer)
+                diff = torch.abs(current_output - self.initial_outputs[name]).sum().item()
+                
+                num_elements = current_output.numel()  # Number of elements in the tensor
+                
+                normalized_diff = diff / num_elements  # Normalized drift
+
+                if diff > threshold:
+                    drifted_layers[name] = {
+                        'total_drift': diff,
+                        'normalized_drift': normalized_diff
+                    }
+
+        return drifted_layers
+
+
+def convert_to_analog_step(model, rpu_config):
     return convert_to_analog(model, rpu_config)
 
 def create_sgd_optimizer_analog(model, lr):
--- test_self_repair.py
+++ test_self_repair.py
@@ -2,9 +2,38 @@ import torch
 
 import os
 
-from lib_digital import create_identity_input, load_images, load_digital_model
-from lib_analog import prepare_digital_to_analog, convert_to_analog
-from config import DEVICE, MODEL_PATH, PATH_DATASET, POS_WEIGHTS_PTH, NEG_WEIGHTS_PTH
+from lib_digital import load_images, load_digital_model
+from lib_analog import prepare_digital_to_analog, convert_to_analog_step, DriftMonitor
+from config import DEVICE, MODEL_PATH, PATH_DATASET, POS_WEIGHTS_PTH, NEG_WEIGHTS_PTH, POS_FACTOR, NEG_FACTOR
+
+def reset_weights_to_nearest_multiplier(layer, layer_name, pos_multipliers_dict, neg_multipliers_dict, pos_factor, neg_factor):
+    import pdb;pdb.set_trace()
+    with torch.no_grad():
+        pos_multipliers = pos_multipliers_dict[layer_name]
+        neg_multipliers = neg_multipliers_dict[layer_name]
+        
+        for name, param in layer.named_parameters():
+            if "weight" in name:
+                flat_weights = param.data.view(-1)
+                flat_pos_multipliers = pos_multipliers.view(-1)
+                flat_neg_multipliers = neg_multipliers.view(-1)
+                
+                for i in range(len(flat_weights)):
+                    flat_weights[i] = flat_pos_multipliers[i] * pos_factor + flat_neg_multipliers[i] * neg_factor
+                
+                param.data = flat_weights.view(param.data.shape)
+
+def calculate_accuracy(model, val_set):
+    predicted_ok = 0
+    total_images = 0
+    for images, labels in val_set:
+        images = images.to(DEVICE)
+        labels = labels.to(DEVICE)
+        pred = model(images)
+        _, predicted = torch.max(pred.data, 1)
+        total_images += labels.size(0)
+        predicted_ok += (predicted == labels).sum().item()
+    return predicted_ok / total_images, total_images
 
 def test_evaluation_self_repairing(model, val_set, pos_multipliers_file, neg_multipliers_file):
     """Test trained network
@@ -15,59 +44,56 @@ def test_evaluation_self_repairing(model, val_set, pos_multipliers_file, neg_mul
     """
     # Save initial state of the model for resetting before each drift operation.
     pos_multipliers_dict = torch.load(pos_multipliers_file, map_location=DEVICE)
+    import pdb;pdb.set_trace()
     neg_multipliers_dict = torch.load(neg_multipliers_file, map_location=DEVICE)
 
-    # Dictionary to store initial values
-    initial_values = {}
+    drift_monitor = DriftMonitor(model)
+    drift_monitor.record_initial_outputs()
 
+    #We do not want to reset the weights
+    #initial_state = model.state_dict()
     model.eval()
-    with torch.no_grad():
-        for name, layer in model.named_children():
-            id_input = create_identity_input(layer)
-            if id_input is not None:
-                x = layer(id_input)
-                initial_values[name] = x.clone()
+    multiple = 600
 
-    initial_state = model.state_dict()
-    model.eval()
-
-    for t_inference in [0.0, 1.0, 20.0, 1000.0, 1e5]:
+    for t_inference in range(1,7):
         # Reset the model to its initial state.
-        model.load_state_dict(initial_state)
+        #model.load_state_dict(initial_state)
         # Apply drift to the weights.
-        model.drift_analog_weights(t_inference)
+        model.drift_analog_weights(multiple*t_inference)
 
         # Setup counter of images predicted to 0.
-        predicted_ok = 0
-        total_images = 0
+        accuracy, total_images = calculate_accuracy(model, val_set)
 
-        for images, labels in val_set:
-            # Predict image.
-            images = images.to(DEVICE)
-            labels = labels.to(DEVICE)
-
-            pred = model(images)
-
-            _, predicted = torch.max(pred.data, 1)
-            total_images += labels.size(0)
-            predicted_ok += (predicted == labels).sum().item()
-
-        print("Number Of Images Tested at t={} = {}".format(t_inference, total_images))
-        print("Model Accuracy at t={} = {}".format(t_inference, predicted_ok / total_images))
-
-    print("\nFinal Number Of Images Tested = {}".format(total_images))
-    print("Final Model Accuracy = {}".format(predicted_ok / total_images))
+        print("Number Of Images Tested at t={} = {}".format(t_inference*multiple, total_images))
+        print("Model Accuracy at t={} = {}".format(t_inference*multiple, accuracy))
+        
+        # Check for drift
+        drifted_layers = drift_monitor.check_drift(threshold=1e-6)
+        # Print drifted layers and their drift metrics
+        reset = False
+        for layer_name, metrics in drifted_layers.items():
+            print(f"Layer {layer_name} - Total Drift: {metrics['total_drift']}, Normalized Drift: {metrics['normalized_drift']}")
+                
+            if metrics['normalized_drift'] > 0.10:
+                print(f"Resetting weights for Layer {layer_name} due to excessive drift.")
+                layer = dict(model.named_children())[layer_name]
+                reset_weights_to_nearest_multiplier(layer, layer_name, pos_multipliers_dict, neg_multipliers_dict, POS_FACTOR, NEG_FACTOR)
+                reset = True
+            
+        if reset:
+            accuracy, total_images = calculate_accuracy(model, val_set)
+            print("Model Accuracy after reset = {}".format(accuracy))
 
 def main():
     """Train a PyTorch analog model with the MNIST dataset."""
     # Load datasets.
     batch_size = 64
-    validation_dataset = load_images(PATH_DATASET, batch_size)
+    _, validation_dataset = load_images(PATH_DATASET, batch_size)
     path = os.path.join(MODEL_PATH, "quantized_mnist.pth")
 
     model = load_digital_model()
     model, rpu_config = prepare_digital_to_analog(model, path)
-    analog_model = convert_to_analog(model, rpu_config)
+    analog_model = convert_to_analog_step(model, rpu_config)
     test_evaluation_self_repairing(analog_model, validation_dataset, POS_WEIGHTS_PTH, NEG_WEIGHTS_PTH)
 
 if __name__ == "__main__":
